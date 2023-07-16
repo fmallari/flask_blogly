297a56881c776fc63d9550c9d42cd544ec887c3e
fmallari <fc.mallari@gmail.com>
2023-07-16 00:09:59 -0700

    initial commit

--- app.py
+++ app.py
@@ -24,61 +24,61 @@ def root():
 
 """User route"""
 
-# @app.route('/users')
-# def users_index():
+@app.route('/users')
+def users_index():
 
-#     users = User.query.order_by(User.last_name, User.first_name).all()
-#     return render_template('users/index.html', users=users)
+    users = User.query.order_by(User.last_name, User.first_name).all()
+    return render_template('users/index.html', users=users)
 
-# @app.route("/users/new", methods=["POST"])
-# def users_new():
-#     """form submission when creating new user"""
+@app.route("/users/new", methods=["POST"])
+def users_new():
+    """form submission when creating new user"""
 
-#     new_user = User(
-#         first_name=request.form['first_name'],
-#         last_name=request.form['last_name'],
-#         image_url=request.form['image_url'] or None)
+    new_user = User(
+        first_name=request.form['first_name'],
+        last_name=request.form['last_name'],
+        image_url=request.form['image_url'] or None)
 
-#     db.session.add(new_user)
-#     db.session.commit()
+    db.session.add(new_user)
+    db.session.commit()
 
-#     return redirect("/users") 
+    return redirect("/users") 
 
-# @app.route('/users/<int:user_id')
-# def users_show(user_id):
-#     """page shoes info on user"""
+@app.route('/users/<int:user_id')
+def users_show(user_id):
+    """page shoes info on user"""
 
-#     user = user.query.get_or_404(user_id)
-#     return render_template('users/show.html', user=user)
+    user = user.query.get_or_404(user_id)
+    return render_template('users/show.html', user=user)
 
-# @app.route('/users/<int:user_id>/edit')
-# def users_edit(user_id):
-#     """show for to edit existing user"""
+@app.route('/users/<int:user_id>/edit')
+def users_edit(user_id):
+    """show for to edit existing user"""
 
-#     user = User.query.get_or_404(user_id)
-#     return render_template('users/edit.html', user=user)
+    user = User.query.get_or_404(user_id)
+    return render_template('users/edit.html', user=user)
 
-# @app.route('/users/<int:user_id>/edit', methods=["POST"])
-# def users_update(user_id):
-#     """form submission for update an existing user"""
+@app.route('/users/<int:user_id>/edit', methods=["POST"])
+def users_update(user_id):
+    """form submission for update an existing user"""
 
-#     user = User.query.get_or_404(user_id)
-#     user.first_name = request.form['first_name']
-#     user.last_name = request.form['last_name']
-#     user.iamge_url = request.form['image_url']
+    user = User.query.get_or_404(user_id)
+    user.first_name = request.form['first_name']
+    user.last_name = request.form['last_name']
+    user.iamge_url = request.form['image_url']
 
-#     db.session.add(user)
-#     db.session.commit()
+    db.session.add(user)
+    db.session.commit()
 
-#     return redirect("/users")
+    return redirect("/users")
 
-# @app.route('/users/<int:user_id>/delete', methods=["POST"])
-# def users_destroy(user_id):
-#     """form submission for deleting an existing user"""
+@app.route('/users/<int:user_id>/delete', methods=["POST"])
+def users_destroy(user_id):
+    """form submission for deleting an existing user"""
 
-#     user = User.query.get_or_404(user_id)
-#     db.session.delete(user)
-#     db.session.commit()
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
 
-#     return redirect("/users")
+    return redirect("/users")
 
